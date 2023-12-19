8236787648faa60260ff4b35df0a8ab96ce595f7
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2023-12-20 04:08:48 +0500

    sacd

---
+++ main.js
@@ -0,0 +1,190 @@
+// Func1
+// function power(a , n){
+//     let res = a ** n;
+//     return res;
+// }
+
+// let a = +prompt(' asonini kiriting');
+// let n = +prompt('n sonini kiriting');
+
+// let res = power(a ,n)
+// console.log(res);
+
+
+
+
+//Func2
+
+// function mean(a , b){
+//     let average = (a + b)/2;
+//     let geometric = Math.sqrt(a + b);
+//     console.log(average);
+//     console.log(geometric);
+// }
+
+// let a = +prompt(' a sonini kiriting');
+// let b = +prompt('b sonini kiriting');
+
+// mean(a , b);
+
+
+//Func3
+
+// function sign(n){
+//     let s = 0;
+//     if(n == 0){
+//         let n = 0;
+//         s += n;
+//     }else if( n >= 0){
+//         let n = 1;
+//         s += n;
+
+//     }else{
+//         let n = -1;
+//         s += n;
+
+//     }
+//     return s;
+// }
+
+// let n = +prompt('n sonini kiriting');
+// let s1 = sign(n);
+// console.log(s1);
+
+
+
+
+//Func4
+// function numberOfRoots(A, B, C){
+//     let D = B ** 2 - 4 * A * C;
+//     let res;
+//     if(D > 0){
+//         res = 2
+//     }else if(D < 0){
+//         res = 0
+//     }else{
+//         res = 1
+//     }
+//     return res;
+// }
+
+// let A = +prompt('A sonini kiriiting');
+// let B = +prompt('B sonini kiriiting');
+// let C = +prompt('C sonini kiriiting');
+
+// let s1= numberOfRoots(A, B, C);
+
+// console.log(s1);
+
+
+
+
+//Func5
+
+// function areaCircle(R){
+//     let S = Math.PI * (R ** 2);
+//     return S;
+// }
+
+// let R = +prompt('Doira raduisini kiriting');
+
+// let S1 = areaCircle(R);
+// console.log(S1);
+
+
+
+
+//Func6
+
+// function sumRange(A, B){
+//       let count = 0;
+//       for(i = A; i <= B; i++){
+//         if(A > B){
+//             count = 0;
+//         }else{
+//             count += i;
+//         }
+//       }
+//       return count;
+// }
+
+// let A = +prompt('A sonini kiriting');
+// let B = +prompt('B sonini kiriting');
+
+// let s1 = sumRange(A ,B);
+// console.log(s1)
+
+
+
+//Func7
+
+// function calc(A, B, S){
+//     let res;
+//     if(S == "-"){
+//         res = A - B;
+//     }else if(S == "+"){
+//         res = A + B;
+//     }else{
+//         res == 0;
+//     }
+//     // if(T = "+"){
+//     //     res = A + B;
+//     // }else{
+//     //     res = 0;
+//     // }
+//     return res;
+// }
+
+// let A = +prompt('A sonini kiriting');
+// let B = +prompt('B sonini kiriting');
+// let S = +prompt('S sonini kiriting');
+
+// let s1 = calc(A,B,S);
+// console.log(s1)
+
+
+
+//Func8
+// function isEven(k){
+//     let res;
+//     if(k % 2 == 0 && k !== 0){
+//         res = true;
+//     }else{
+//         res = false;
+//     }
+//     return res;
+// }
+
+// let k = +prompt('k sonini kiriting');
+
+// let Ev1 = isEven(k);
+// console.log(Ev1);
+
+
+
+
+//Func9
+// function sortABC(a, b, c){
+//     let res;
+//     if(a > b && b > c){
+//         res = a , b , c;
+//         if(a > b && b < c){
+//             res = a , c ,b;
+//         }
+//     }
+
+//     return res;
+// }
+
+// let s1 = sortABC(5,3,4);
+// console.log(s1);
+
+
+
+
+//Func10
+// function  isPowerN(K, N){
+//     let res = Math.SQRT1_2(K);
+//     let s1 = Number.isInteger(res);
+//     if(){}
+// }
